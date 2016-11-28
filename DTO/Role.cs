b25303e8ca25017ebeca436b7a338d82105cb6f7
NhatVu <minhnhatse.uit@gmail.com>
2016-11-28 08:35:06 +0700

    initialize project

---
+++ DTO/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class Role
+    {
+        public string Id { get; set; }
+        public string RoleName { get; set; }
+
+        public Role() { }
+
+        public Role(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+    }
+}
