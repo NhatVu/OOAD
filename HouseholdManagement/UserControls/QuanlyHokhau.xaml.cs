2f9c2662e9d09eb94f0f77e0fe2fa29dcd0c324b
Bee <lazybee27102@gmail.com>
2016-12-19 07:39:02 -0800

    Fundamental User Interface stuff

---
+++ HouseholdManagement/UserControls/QuanlyHokhau.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HouseholdManagement.UserControls
+{
+    /// <summary>
+    /// Interaction logic for QuanlyHokhau.xaml
+    /// </summary>
+    public partial class QuanlyHokhau : UserControl
+    {
+        public static QuanlyHokhau createInstance()
+        {
+            return new QuanlyHokhau();
+        }
+
+        public QuanlyHokhau()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+    }
+}
