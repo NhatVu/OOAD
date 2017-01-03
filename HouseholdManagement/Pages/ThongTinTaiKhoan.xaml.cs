1c971e2bc0ef008a185c088817acb3b89ecd2818
NhatVu <minhnhatse.uit@gmail.com>
2017-01-03 11:19:44 +0700

    xong chuc nang ThongTinTaiKhoan

--- HouseholdManagement/Pages/ThongTinTaiKhoan.xaml.cs
+++ HouseholdManagement/Pages/ThongTinTaiKhoan.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DTO;
+using DataAcessLayer;
+using HouseholdManagement.Utilities;
+using System.Data;
 
 namespace HouseholdManagement.Pages
 {
@@ -20,6 +24,8 @@ namespace HouseholdManagement.Pages
     /// </summary>
     public partial class ThongTinTaiKhoan : Page
     {
+        CongDanDAO congDanDAO = null;
+        DanTocDAO danTocDAO = null;
         public static ThongTinTaiKhoan createInstance()
         {
             return new ThongTinTaiKhoan();
@@ -28,6 +34,19 @@ namespace HouseholdManagement.Pages
         public ThongTinTaiKhoan()
         {
             InitializeComponent();
+            congDanDAO = new CongDanDAO();
+            danTocDAO = new DanTocDAO();
         }
+
+        private void onLoaded(object sender, RoutedEventArgs e)
+        {
+            DataTable congDanSource = congDanDAO.SelectCongDanById(GlobalVariable.CurrentCongAnId);
+            CongDanDTO dto = Constant.DataTableToList<CongDanDTO>(congDanSource)[0];
+            DataTable danTocSource = danTocDAO.SelectDanTocById(dto.IdDantoc);
+            this.DataContext = dto;
+            this.gioiTinh.Text = (dto.GioiTinh == 0) ? "Nữ" : "Nam";
+            this.textbox_danToc.Text = danTocSource.Rows[0]["tenDanToc"].ToString();
+        }
+
     }
 }
